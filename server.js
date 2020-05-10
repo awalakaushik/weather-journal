4c76d95128af759a053723e32b78748707932d3d
awalakaushik <awalakaushik@gmail.com>
2020-05-10 16:06:18 -0500

    add api calls for server

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 const cors = require('cors');
+const { response } = require('express');
 app.use(cors());
 
 /* Initializing the main project folder */
@@ -24,9 +25,69 @@ app.use(express.static('website'));
 
 const port = 8000;
 
+// Spin up the server
+const server = app.listen(port, serverListener);
+
 function serverListener() {
     console.log(`Server is listening on port ${port}`);
 }
 
-// Spin up the server
-const server = app.listen(port, serverListener);
+
+/**
+ * Route Endpoints
+ */
+
+app.get('/api/projectData', getProjectData);
+
+app.post('/api/projectData', postProjectData);
+
+/**
+ * Endpoint callback functions
+ */
+
+function getProjectData (req, res) {
+    res.send(projectData);
+}
+
+async function postProjectData (req, ress) {
+    const data = req.body;
+    
+    // return if there is no post data
+    if (!data.zip && data.userFeeling) {
+        const error = "Invalid data: Zip and User Feeling are required!";
+        console.log(error);
+        res.send(error);
+    }
+
+    // post data otherwise
+    try {
+        const weatherData = await fetchWeatherInformation(data.zip);
+
+        const tempInFahrenheit = (1.8 * (weatherData.main.temp - 273) + 32).toFixed(1); // 1.8(K - 273) + 32.
+
+        projectData = {
+            zipCode: data.zip,
+            userFeeling: data.userFeeling,
+            degreeFarenheit: tempInFahrenheit
+        };
+
+        res.send(projectData);
+    }
+    catch (error) {
+        console.log(`Error while posting data: ${error}`);
+    }
+}
+
+
+const fetchWeatherInformation = async (weatherQuery) => {
+
+    try {
+        const weatherApiResponse = await fetch(`${openWeatherApi.baseUrl}q=${weatherQuery}&appid=${openWeatherApi.apiKey}`);
+        const data = await weatherApiResponse.json();
+        return data;
+    }
+    catch (error) {
+        console.log(`Error while fetching weather data: ${error}`)
+    }
+
+};
